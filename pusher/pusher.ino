3ba0abc8178f54d273619e3543953d372b372e82
Peter Clarke <petermclarke@gmail.com>
2018-01-09 21:30:39 +0000

    Subscribe to the channels in the device config file

--- pusher/pusher.ino
+++ pusher/pusher.ino
@@ -1,18 +1,19 @@
 
 /*
- * Pusher client for the Dennis Signage application
+ * WebSocketClient.ino
  *
  */
 
 #include <Arduino.h>
 
 // Installed via the Arduino library manager.
+#include <ArduinoJson.h>
 #include <ESP8266WiFi.h>
 #include <ESP8266WiFiMulti.h>
+#include <ESP8266HTTPClient.h>
 #include <WebSocketsClient.h>
-#include <ArduinoJson.h>
 
-// Rename example_config.h to config.h
+
 #include "config.h"
 
 ESP8266WiFiMulti WiFiMulti;
@@ -20,13 +21,15 @@ WebSocketsClient webSocket;
 
 #define JSON_BUFFER_SIZE 1024
 
+char configured = 0;
+char pusher_ready = 0;
+
 
 String pusherSubscribeJsonString(char * channel) {
   String json = "{\"event\":\"pusher:subscribe\",\"data\": {\"channel\": \"_C_\"}}";
   json.replace(String("_C_"), String(channel));
   return json;
 }
-
 void onPusherConnectionEstablished(const char* data) {
   StaticJsonBuffer<JSON_BUFFER_SIZE> jsonBuffer;
   JsonObject& jsonObj = jsonBuffer.parseObject(data);
@@ -35,9 +38,17 @@ void onPusherConnectionEstablished(const char* data) {
     const char* activity_timeout = jsonObj["activity_timeout"];
     webSocket.setReconnectInterval(atoi(activity_timeout) * 1000);
 
-    // Subscribe to the initial channel.
-    String json = pusherSubscribeJsonString("my-channel");
-    webSocket.sendTXT(json);
+    pusher_ready = 1;
+  }
+}
+
+void onMessage(const char* data) {
+  StaticJsonBuffer<JSON_BUFFER_SIZE> jsonBuffer;
+  JsonObject& jsonObj = jsonBuffer.parseObject(data);
+
+  if (jsonObj.success()) {
+    const char* title = jsonObj["title"];
+    Serial.print("title: "); Serial.println(title);
   }
 }
 
@@ -70,9 +81,13 @@ void handlePusherEvent(uint8_t * payload) {
   if (strcmp(event, "pusher:connection_established") == 0) {
     onPusherConnectionEstablished(data);
   }
+  else if (strcmp(event, "message") == 0) {
+    onMessage(data);
+  }
   else if (strcmp(event, "my-event") == 0) {
     onMyEvent(data);
   }
+  // 
     
 }
 
@@ -94,7 +109,53 @@ void webSocketEvent(WStype_t type, uint8_t * payload, size_t length) {
 void signageConfigure() {
   // Get the config json from SIGNAGE_CONFIG_URL
 
+  String payload;
+
+  HTTPClient http;
+  Serial.print("[HTTP] begin...\n");
+  // configure traged server and url
+  http.begin(SIGNAGE_CONFIG_URL);
+
+  Serial.print("[HTTP] GET...\n");
+  // start connection and send HTTP header
+  int httpCode = http.GET();
+
+  // httpCode will be negative on error
+  if(httpCode > 0) {
+      // HTTP header has been send and Server response header has been handled
+      Serial.printf("[HTTP] GET... code: %d\n", httpCode);
+
+      // file found at server
+      if(httpCode == HTTP_CODE_OK) {
+          payload = http.getString();
+          Serial.println(payload);
+      }
+  } else {
+      Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
+  }
+
+  http.end();
+  
+
   // Subscribe to the Pusher channels listed in the config.
+  StaticJsonBuffer<JSON_BUFFER_SIZE> jsonBuffer;
+  JsonObject& jsonObj = jsonBuffer.parseObject(payload);
+
+  if (jsonObj.success()) {
+    const char* channel_channel = jsonObj["channels"][0]["channel_name"];
+    Serial.print("channel_channel: "); Serial.println(channel_channel);
+    const char* channel_control = jsonObj["channels"][1]["channel_name"];
+    Serial.print("channel_control: "); Serial.println(channel_control);
+
+    // Subscribe to the 2 channels.
+    String control = pusherSubscribeJsonString("channel_control");
+    webSocket.sendTXT(control);
+
+    String channel = pusherSubscribeJsonString("channel_channel");
+    webSocket.sendTXT(channel);
+
+    configured = 1;
+  }
   
 }
 
@@ -118,13 +179,21 @@ void setup() {
   webSocket.onEvent(webSocketEvent);
 
   // try ever 5000 again if connection has failed
-  webSocket.setReconnectInterval(120000);
+  webSocket.setReconnectInterval(5000);
 
   // Config the device according to the dennis signage config page.
-  signageConfigure();
+  //signageConfigure();
+
 }
 
 void loop() {
   webSocket.loop();
+
+  if (!configured && pusher_ready) {
+    // Config the device according to the dennis signage config page.
+    signageConfigure();
+  }
+  
 }
+
 
